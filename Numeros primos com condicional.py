78f3929c6feb5583645569c221b5242f52ba1f18
Michel <164029428+amorais-michel@users.noreply.github.com>
2024-03-20 17:03:17 -0300

    Add files via upload

---
+++ Numeros primos com condicional.py
@@ -0,0 +1,7 @@
+numero_dividir = int(input("Digite um numero para testar"))
+if ( numero_dividir % 2) != 0:
+    print ("O numero", numero_dividir, "é primo" )
+if numero_dividir % 2 == 0:
+    print("nao primo")
+
+
